a6377b41375105ec7112b68182199da7fe4acf47
Cagri Sofuoglu <cagri.sofuoglu@gmail.com>
2017-08-30 17:41:46 +0300

    typo fix.

--- jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter_notebook_config.py
@@ -1,7 +1,7 @@
 # Set options for certfile, ip, password, and toggle off
 # browser auto-opening
 c.NotebookApp.certfile = u'/home/ds/.ssl/mycert.pem'
-c.NotebookApp.keyfile = u'/home/ds/.ssl//mykey.key'
+c.NotebookApp.keyfile = u'/home/ds/.ssl/mykey.key'
 # Set ip to '*' to bind on all interfaces (ips) for the public server
 c.NotebookApp.ip = '*'
 c.NotebookApp.password = u'sha1:c2b4768e9f2f:ae829e1c0b143757ef3cea54e7b6749087164158'
