a3a683f69e1b7efb7c060083ba576d3d5573bc8d
Cagri Sofuoglu <cagri.sofuoglu@gmail.com>
2018-10-12 15:40:04 +0000

    Abandoned my_init.

--- services/run_ipython.sh
+++ services/run_ipython.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-
-exec /sbin/setuser jpy3 /opt/jpy3/bin/jupyter lab --no-browser --port 8888 --ip=0.0.0.0 $*
+/opt/jpy3/bin/jupyter lab --no-browser --port 8888 --ip=0.0.0.0 $*
--- services/run_tensorboard.sh
+++ services/run_tensorboard.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-exec /sbin/setuser jpy3 /opt/jpy3/bin/tensorboard --logdir=/tmp/tf/logs/
+/opt/jpy3/bin/tensorboard --logdir=/tmp/tf/logs/
 
